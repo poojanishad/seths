7531a5eafc1bd529a5da90d667e148b9e34c0166
Pooja Nishad <poojanishad0306@gmail.com>
2026-04-14 22:26:48 +0530

    fix: remove node-fetch (use native fetch)

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express   from 'express'
 import cors      from 'cors'
 import dotenv    from 'dotenv'
 import nodemailer from 'nodemailer'
-import fetch from 'node-fetch' 
 
 dotenv.config()
 
